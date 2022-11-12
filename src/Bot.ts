3978695a7575a01ca61218984b9a3bf63190d935
Lak Moore <stuart.rickard@after-ice.com>
2022-11-12 20:52:28 +0000

    Working Hello World example Bot

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,5 +1,7 @@
 import * as dotenv from 'dotenv';
 import { Client } from "discord.js";
+import ready from "./listeners/ready";
+import interactionCreate from './listeners/interactionCreate';
 
 dotenv.config()
 console.log("Bot is starting...");
@@ -7,6 +9,10 @@ console.log("Bot is starting...");
 const client = new Client({
     intents: []
 });
+
+ready(client);
+interactionCreate(client);
+
 client.login(process.env.SECRET_TOKEN);
 
 console.log(client);
