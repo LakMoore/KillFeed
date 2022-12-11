746e7107fc56a1991f4f8445c5720359e9d01107
Lak Moore <6624290+LakMoore@users.noreply.github.com>
2022-12-11 22:53:02 +0000

    try a different solution

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,20 +2,10 @@ module.exports = {
   apps: [
     {
       name: "KillFeed",
-      script: "ts-node src/Bot.ts",
+      script: "npm",
+      args: "run start",
       instances: 1,
       autorestart: true,
     },
-  ],
-  deploy: {
-    production: {
-      user: "lakm",
-      host: "dnet-nyc3do",
-      path: "/home/lakm/KillFeed",
-      repo: "git@github.com:LakMoore/KillFeed.git",
-      ref: "origin/main",
-      key: "deploy.key",
-      "post-deploy": "npm i; pm2 reload ecosystem.config.js --env production",
-    },
-  },
+  ]
 };
