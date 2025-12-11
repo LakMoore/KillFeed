80e4e375431b7ca4eed3361c620a8977428e00b1
Lak Moore <6624290+LakMoore@users.noreply.github.com>
2025-12-11 15:59:30 +0000

    Process channels in sequence using async/await

--- src/Servers.ts
+++ src/Servers.ts
@@ -2,23 +2,21 @@ import { Client } from "discord.js";
 import { updateChannel } from "./Channels";
 import { canUseChannel } from "./helpers/DiscordHelper";
 
-export function updateGuild(
+export async function updateGuild(
   client: Client<boolean>,
   guildId: string,
   guildName: string
 ) {
   // Fetch the guild/server by ID
-  return client.guilds.fetch(guildId).then((g) => {
-    // Fetch all channels from this guild/server
-    return g.channels.fetch().then((c) => {
-      // This is an async "foreach"
-      return Promise.all(
-        c.map((chn) => {
-          if (canUseChannel(chn)) {
-            return updateChannel(client, chn.id, guildName);
-          }
-        })
-      );
-    });
-  });
+  const g = await client.guilds.fetch(guildId);
+
+  // Fetch all channels from this guild/server
+  const c = await g.channels.fetch();
+
+  for (const ch of c) {
+    const chn = ch[1];
+    if (canUseChannel(chn)) {
+      await updateChannel(client, chn.id, guildName);
+    }
+  }
 }
