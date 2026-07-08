7b19f9dcbc43ee706679aa690bc8d4f7f91cee4d
Lak Moore <6624290+LakMoore@users.noreply.github.com>
2026-07-08 13:12:05 +0100

    Try to force commands to update immediately on publish

--- src/Servers.ts
+++ src/Servers.ts
@@ -1,6 +1,7 @@
 import type { Client } from 'discord.js';
 import { updateChannel } from './Channels';
 import { canUseChannel } from './helpers/DiscordHelper';
+import { Commands } from './Commands';
 
 export async function updateGuild(
   client: Client<boolean>,
@@ -9,6 +10,8 @@ export async function updateGuild(
 ) {
   // Fetch the guild/server by ID
   const g = await client.guilds.fetch(guildId);
+
+  await g.commands.set(Commands); // Overwrite the commands for this guild to ensure it has latest
 
   // Fetch all channels from this guild/server
   const c = await g.channels.fetch();
